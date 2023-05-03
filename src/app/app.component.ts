b25b74d366f620bf3a7f70ff6120283bfebb125e
Peter Silling <peter.silling@proximate.dev>
2023-05-03 11:34:25 +0200

    firebasedeploy

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { initializeApp } from 'firebase/app';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +7,19 @@ import { Component } from '@angular/core';
   styleUrls: ['app.component.scss'],
 })
 export class AppComponent {
-  constructor() {}
+  constructor() {
+    const firebaseConfig = {
+      apiKey: 'AIzaSyB-XUkVfqQx-vNG-YoycAIYv01uaG7dcy0',
+      authDomain: 'timesheet-4c6b4.firebaseapp.com',
+      databaseURL:
+        'https://timesheet-4c6b4-default-rtdb.europe-west1.firebasedatabase.app',
+      projectId: 'timesheet-4c6b4',
+      storageBucket: 'timesheet-4c6b4.appspot.com',
+      messagingSenderId: '852554831804',
+      appId: '1:852554831804:web:909d5537b47dea07963db9',
+    };
+
+    // Initialize Firebase
+    const app = initializeApp(firebaseConfig);
+  }
 }
